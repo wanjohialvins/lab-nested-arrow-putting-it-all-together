649e604ec67574df6e8fc83a7cef860d60230058
wanjohialvins <wanjohialvins@gmail.com>
2025-11-27 21:34:37 +0300

    nested if and arrow funcions

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+// createLoginTracker function
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    attemptCount++;
+
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+}
 
 
 module.exports = {
